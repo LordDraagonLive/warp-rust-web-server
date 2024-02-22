dfc819b151661c276e7f734933523aa836904c74
Buddhi Adhikari <buddhi.adhikari@churchillcap.com>
2024-02-23 01:22:31 +0530

    finalizing a proper server

--- src/main.rs
+++ src/main.rs
@@ -5,16 +5,32 @@ fn main() {
 
     for mut stream in listener.incoming().flatten() {
         let mut rdr = std::io::BufReader::new(&mut stream);
+        let mut l: String = String::new();
+        rdr.read_line(&mut l).unwrap();
 
-        loop {
-            let mut l = String::new();
-            rdr.read_line(&mut l).unwrap();
-            if l.trim().is_empty() {
-                break;
+        match l.trim().split(' ').collect::<Vec<_>>().as_slice() {
+            ["GET", resource, "HTTP/1.1"] => {
+                loop {
+                    let mut l = String::new();
+                    rdr.read_line(&mut l).unwrap();
+                    if l.trim().is_empty() {
+                        break;
+                    }
+                }
+                let mut p = std::path::PathBuf::new();
+                
+                p.push("htdocs");
+                p.push(resource.trim_start_matches("/"));
+                if resource.ends_with('/') {
+                    p.push("index.html");
+                }
+                // println!("Attempting to read file at path: {:?}", p);
+
+                
+                stream.write_all(b"HTTP/1.1 200 OK\r\n\r\n").unwrap();
+                stream.write_all(&std::fs::read(p).unwrap()).unwrap();
             }
-            print!("{l}");
+            _ => todo!(),
         }
-
-        stream.write_all(b"HTTP/1.1 200 OK \r\n\r\nHello Computer!").unwrap();
     }
 }
